2a79108f428cdc45f60fb2f796457e25aeebd24c
jamesosull16 <jamesosull16@gmail.com>
2020-11-20 08:51:22 -0700

    formatting for badge link

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -11,7 +11,7 @@ function generateBadge(data) {
       return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
 
     case "IBM Public License Version 1.0":
-      return "[![License: IPL]](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      return "[![License: IPL](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
 
     case "ISC License":
       return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
